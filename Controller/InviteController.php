d9810c5acfa8ae3580b51518206bc5e9a1d2f70c
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-05-02 11:39:15 +0200

    Send invites via mail to an user

--- Controller/InviteController.php
+++ Controller/InviteController.php
@@ -88,15 +88,66 @@ class InviteController extends Controller
 
         if (!$invite) {
             $this->get('session')->getFlashBag()->add('error', sprintf('The invite %s does not exist.', $code));
-        } else {
-            if ($invite->getUser()) {
-                $this->get('session')->getFlashBag()->add('error', sprintf('The invite %s has already been used by %s. You can\'t delete an used invite.', $invite->getCode(), $invite->getUser()));
-            } else {
-                $manager->deleteInvite($invite);
-
-                $this->get('session')->getFlashBag()->add('success', sprintf('The invite %s has been deleted.', $invite->getCode()));
-            }
+            return $this->redirect($this->generateUrl('bc_user_admin_invite_list'));
         }
+
+        if ($invite->getUser()) {
+            $this->get('session')->getFlashBag()->add('error', sprintf('The invite %s has already been used by %s. You can\'t delete an used invite.', $invite->getCode(), $invite->getUser()));
+            return $this->redirect($this->generateUrl('bc_user_admin_invite_list'));
+        }
+
+        $manager->deleteInvite($invite);
+
+        $this->get('session')->getFlashBag()->add('success', sprintf('The invite %s has been deleted.', $invite->getCode()));
+
+        return $this->redirect($this->generateUrl('bc_user_admin_invite_list'));
+    }
+
+    public function sendAction($code)
+    {
+        $manager = $this->get('bc_user.invite_manager');
+        $invite = $manager->findInviteByCode($code);
+
+        if (!$invite) {
+            $this->get('session')->getFlashBag()->add('error', sprintf('The invite %s does not exist.', $code));
+            return $this->redirect($this->generateUrl('bc_user_admin_invite_list'));
+        }
+
+        if (!$invite->getEmail()) {
+            $this->get('session')->getFlashBag()->add('error', sprintf('The invite %s does not have an email address associated and can\'t be sent.', $code));
+            return $this->redirect($this->generateUrl('bc_user_admin_invite_list'));
+        }
+
+        if ($invite->getUser()) {
+            $this->get('session')->getFlashBag()->add('error', sprintf('The invite %s has already been used by %s. You can\'t send an used invite.', $invite->getCode(), $invite->getUser()));
+            return $this->redirect($this->generateUrl('bc_user_admin_invite_list'));
+        }
+
+        if ($invite->isSent()) {
+            $this->get('session')->getFlashBag()->add('error', sprintf('The invite %s has already been sent to the email address %s. You can\'t send an used invite.', $invite->getCode(), $invite->getEmail()));
+            return $this->redirect($this->generateUrl('bc_user_admin_invite_list'));
+        }
+
+        $message = \Swift_Message::newInstance()
+                ->setSubject('Invite for BcUserAdmin')
+                ->setFrom('useradmin@braincrafted.com')
+                ->setTo($invite->getEmail())
+                ->setBody(
+                    $this->renderView(
+                        'BcUserAdminBundle:Email:invite.txt.twig',
+                        array(
+                            'email' => $invite->getEmail(),
+                            'code'  => $invite->getCode()
+                        )
+                    )
+                )
+            ;
+        $this->get('mailer')->send($message);
+
+        $invite->send();
+        $manager->updateInvite($invite);
+
+        $this->get('session')->getFlashBag()->add('success', sprintf('The invite %s has been sent to %s.', $invite->getCode(), $invite->getEmail()));
 
         return $this->redirect($this->generateUrl('bc_user_admin_invite_list'));
     }
