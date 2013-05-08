d37987f95b0e4858c06303e98d129d5f3a0fa479
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-05-08 13:15:43 +0200

    Started with some PHPUnit tests

--- Tests/Controller/InviteControllerTest.php
+++ Tests/Controller/InviteControllerTest.php
@@ -25,42 +25,92 @@ class InviteControllerTest extends WebTestCase
 {
     public function setUp()
     {
-        $this->setUpKernel();
+        // $this->setUpKernel();
     }
 
     public function tearDown()
     {
-        $this->tearDownKernel();
+        // $this->tearDownKernel();
     }
 
     /**
-     * @covers Bc\Bundle\UserAdminBundle\Controller\InviteController::listAction()
+     * Tests `listAction()`.
+     *
+     * @covers Bc\Bundle\UserAdminBundle\Controller\InviteController
      */
-    public function testList()
+    public function testListAction()
     {
-        $client = $this->createClient();
-        $this->login($client);
-
-        $client->followRedirects();
-        $crawler = $client->request('GET', '/invite');
-        $this->assertGreaterThan(0, $crawler->filter('h1:contains("Invites")')->count());
+        $this->markTestIncomplete('Need to add AppKernel before this test can run.');
     }
 
     /**
-     * Login.
+     * Tests `createAction()`.
      *
-     * @param Client $client The client
-     *
-     * @return void
+     * @covers Bc\Bundle\UserAdminBundle\Controller\InviteController
      */
-    protected function login(Client $client)
+    public function testCreateAction()
     {
-        $crawler = $client->request('GET', '/login');
-        $button = $crawler->selectButton('_submit');
-        $form = $button->form(array(
-            '_username' => 'admin',
-            '_password' => 'admin'
-        ));
-        $client->submit($form);
+        $this->markTestIncomplete('Need to add AppKernel before this test can run.');
     }
+
+    /**
+     * Tests `batchCreateAction()`.
+     *
+     * @covers Bc\Bundle\UserAdminBundle\Controller\InviteController
+     */
+    public function testBatchCreateAction()
+    {
+        $this->markTestIncomplete('Need to add AppKernel before this test can run.');
+    }
+
+    /**
+     * Tests `sendAction()`.
+     *
+     * @covers Bc\Bundle\UserAdminBundle\Controller\InviteController
+     */
+    public function testSendAction()
+    {
+        $this->markTestIncomplete('Need to add AppKernel before this test can run.');
+    }
+
+    /**
+     * Tests `deleteAction()`.
+     *
+     * @covers Bc\Bundle\UserAdminBundle\Controller\InviteController
+     */
+    public function testDeleteAction()
+    {
+        $this->markTestIncomplete('Need to add AppKernel before this test can run.');
+    }
+
+    // /**
+    //  * @covers Bc\Bundle\UserAdminBundle\Controller\InviteController::listAction()
+    //  */
+    // public function testList()
+    // {
+    //     $client = $this->createClient();
+    //     $this->login($client);
+
+    //     $client->followRedirects();
+    //     $crawler = $client->request('GET', '/invite');
+    //     $this->assertGreaterThan(0, $crawler->filter('h1:contains("Invites")')->count());
+    // }
+
+    // /**
+    //  * Login.
+    //  *
+    //  * @param Client $client The client
+    //  *
+    //  * @return void
+    //  */
+    // protected function login(Client $client)
+    // {
+    //     $crawler = $client->request('GET', '/login');
+    //     $button = $crawler->selectButton('_submit');
+    //     $form = $button->form(array(
+    //         '_username' => 'admin',
+    //         '_password' => 'admin'
+    //     ));
+    //     $client->submit($form);
+    // }
 }
